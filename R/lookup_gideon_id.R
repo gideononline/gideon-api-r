6078a85aac5fe37f6b1db90d911021277bd51aa8
Xander Hirsch <xander@gideononline.com>
2021-06-28 15:21:38 -0700

    Provided more robust user input error checking

--- R/lookup_gideon_id.R
+++ R/lookup_gideon_id.R
@@ -13,19 +13,46 @@ gideon_category_path_id_name <- list(
   regions = c('/travel/regions', 'region_code', 'region')
 )
 
-#' GIDEON Item ID Lookup
+#' GIDEON Item Identification Code Lookup
 #'
-#' Looks up the ID for a disease, bacteria, etc. in the GIDEON database.
+#' Finds the ID for a disease, bacteria, etc. in the GIDEON database.
 #' HINT: Copy and paste the item name from the GIDEON dashboard.
 #'
-#' @param category One of the following: diseases, drugs, vaccines, agents,
-#'     vectors, vehicles, reservoirs, bacteria, mycobacteria, yeasts,
-#'     countries, regions
-#' @param item The name of the item
-#' @param fail_silently Only return NULL instead of all options.
+#' @param category A string that is one of the following categories: diseases,
+#'     drugs, vaccines, agents, vectors, vehicles, reservoirs, bacteria,
+#'     mycobacteria, yeasts, countries, regions
+#' @param item A string of the name of the item to search. If this parameter is
+#'     omitted, the function will return a dataframe representing all items and
+#'     related GIDEON identification codes.
+#' @param error_msg Throw an error message if the lookup does not find the
+#'     specified item rather than returning \code{NULL}.
+#'
+#' @return
+#' \itemize{
+#'  \item If the \emph{item parameter matches} an entry from the item category,
+#'      the string or integer code will be returned.
+#'  \item If the \emph{item parameter fails to match} an entry from the item
+#'      category, \code{NULL} will be returned.
+#'  \item If the \emph{item parameter is omitted}, return a dataframe with all
+#'      items in the category, including the name and code.
+#' }
+#'
+#'
+#'
+#' @examples
+#' \donttest{
+#' # Find the disease code given the name.
+#' ebola_code <- lookup_gideon_id("diseases", "Ebola")
+#'
+#' # Find a country code given the country name
+#' us_code <- lookup_gideon_id("countries", "United States")
+#'
+#' # List all the disease vectors
+#' all_vectors <- lookup_gideon_id("vectors")
+#' }
 #'
 #' @export
-lookup_gideon_id <- function(category, item, fail_silently = TRUE) {
+lookup_gideon_id <- function(category, item = NULL, error_msg = TRUE) {
   # Ensure the category is from the list
   if (!is.element(category, names(gideon_category_path_id_name))) {
     stop(paste('Category "',
@@ -39,14 +66,28 @@ lookup_gideon_id <- function(category, item, fail_silently = TRUE) {
   item_id <- gideon_category_path_id_name[[category]][2]
   item_name <- gideon_category_path_id_name[[category]][3]
 
-  matches <- dplyr::filter(gideon::query_gideon_api(api_path),
-                           !!dplyr::sym(item_name) == item)
+  full_item_list <- gideon::query_gideon_api(api_path)
+
+  if (is.null(item)) {
+    return(full_item_list[1:2])
+  }
+
+  matches <- dplyr::filter(full_item_list, !!dplyr::sym(item_name) == item)
 
   if (nrow(matches)==1) {
     return(matches[[item_id]])
   }
-  if (fail_silently) {
-    return(NULL)
+  if (error_msg) {
+    stop(paste("GIDEON item ID code lookup could not find any matches for ",
+               "'",
+               item,
+               "'",
+               " in the ",
+               "'",
+               category,
+               "'",
+               " category",
+               sep = ""))
   }
-  return(matches[1:2])
+
 }
--- R/query_gideon_api.R
+++ R/query_gideon_api.R
@@ -5,18 +5,42 @@
 #' @param query Additional URL query parameters to send as a named list
 #' @param to_dataframe Automatically convert the response to a dataframe.
 #'
+#' @returns
+#' A dataframe representing the response or a string of the JSON response if
+#' \code{to_dataframe} is set to \code{FALSE}
+#'
+#' @seealso \url{https://api-doc.gideononline.com/}
+#'
 #' @export
 query_gideon_api <- function(path, query = NULL, to_dataframe = TRUE){
+  user_api_key <- Sys.getenv('GIDEON_API_KEY')
+  if (user_api_key == "") {
+    stop(paste("Cannot find GIDEON_API_KEY environment variable.",
+               "Set the GIDEON_API_KEY variable in your .Renviron file"))
+  }
+
   r <- httr::GET(
     paste('https://api.gideononline.com', path, sep = ''),
     query = query,
-    httr::add_headers(Authorization = paste('api_key',
-                                            Sys.getenv('GIDEON_API_KEY')))
+    httr::add_headers(Authorization = paste('api_key', user_api_key))
   )
-  if (httr::status_code(r)==200){
+
+  resp_status_code <- httr::status_code(r)
+  if (resp_status_code == 200) {
     if (to_dataframe) {
       return(jsonlite::fromJSON(httr::content(r, 'text'))$data)
     }
     return(httr::content(r, 'text'))
   }
+  if (resp_status_code == 401) {
+    stop(paste("The GIDEON_API_KEY environment variable provided is not a",
+               "valid GIDEON API key"))
+  }
+  if (resp_status_code == 404) {
+    stop(paste("'", path, "'", " is not a valid GIDEON API path", sep = ""))
+  }
+
+  stop(paste("GIDEON API query returned a",
+             resp_status_code,
+             "HTTP status code"))
 }
