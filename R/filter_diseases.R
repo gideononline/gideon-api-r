77c398b3d38d823c32bb7f1555fa23371d871bab
Xander Hirsch <xander@gideononline.com>
2021-07-02 11:44:15 -0700

    Small documentation fixes

--- R/filter_diseases.R
+++ R/filter_diseases.R
@@ -5,15 +5,17 @@
 #' @param vector GIDEON vector code - An arthropod or other living carrier which
 #'  transports an infectious agent from an infected organism or reservoir to a
 #'  susceptible individual or immediate surroundings.
-#' @param vehicle The mode of transmission for an infectious agent. This
-#'  generally implies a passive and inanimate (i.e., non-vector) mode.
-#' @param reservoir Any animal, arthropod, plant, soil or substance in which an
-#'  infectious agent normally lives and multiplies, on which it depends
-#'  primarily for survival, and where it reproduces itself in such a manner that
-#'  it can be transmitted to a susceptible host.
-#' @param country In general, this list of country names corresponds to accepted
-#'  geographical and political designations. When Country filter is used, it
-#'  will only retrieve diseases that are endemic to the country.
+#' @param vehicle GIDEON vehicle code - The mode of transmission for an
+#'  infectious agent. This generally implies a passive and inanimate
+#'  (i.e., non-vector) mode.
+#' @param reservoir GIDEON reservoir code - Any animal, arthropod, plant, soil
+#'  or substance in which an infectious agent normally lives and multiplies, on
+#'  which it depends primarily for survival, and where it reproduces itself in
+#'  such a manner that it can be transmitted to a susceptible host.
+#' @param country GIDEON country code - In general, this list of country names
+#'  corresponds to accepted geographical and political designations. When
+#'  Country filter is used, it will only retrieve diseases that are endemic to
+#'  the country.
 #' @seealso \url{https://api-doc.gideononline.com/#4272d285-ba04-435d-b7ad-deabe971330e}
 #' @seealso \code{\link{lookup_gideon_id}}
 #'
